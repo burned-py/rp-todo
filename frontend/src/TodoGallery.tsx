6dd2b19f3bd66a3b69f21623411190d57877c6df
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2023-03-27 10:22:02 +0200

    Add TodoGallery Collumns

--- frontend/src/TodoGallery.tsx
+++ frontend/src/TodoGallery.tsx
@@ -1,3 +1,4 @@
+import './TodoGallery.css'
 import {Todo} from "./Todo";
 import TodoCard from "./TodoCard";
 
@@ -7,11 +8,28 @@ type Props = {
 
 export default function TodoGallery(props: Props) {
 
+    const openTodos: Todo[] = props.todos.filter((todo) => todo.status === 'OPEN')
+    const inProgressTodos: Todo[] = props.todos.filter((todo) => todo.status === 'IN_PROGRESS')
+    const doneTodos: Todo[] = props.todos.filter((todo) => todo.status === 'DONE')
+
+
     return (
-        <div>
-            {
-                props.todos.map((todo) => <TodoCard key={todo.id} todo={todo} />)
-            }
+        <div className='todo-gallery'>
+            <div className='todo-gallery_column'>
+                {
+                    openTodos.map((todo) => <TodoCard key={todo.id} todo={todo} />)
+                }
+            </div>
+            <div className='todo-gallery_column'>
+                {
+                    inProgressTodos.map((todo) => <TodoCard key={todo.id} todo={todo} />)
+                }
+            </div>
+            <div className='todo-gallery_column'>
+                {
+                    doneTodos.map((todo) => <TodoCard key={todo.id} todo={todo} />)
+                }
+            </div>
         </div>
     )
 }
